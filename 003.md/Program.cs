09f2ced773e17f6c87e973ea7f971026bf8a5c6b
vlasenkova <asyicheva1@mail.ru>
2022-08-07 14:10:32 +0300

    третья задача

---
+++ 003.md/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA % 2==0) {
+    Console.WriteLine($"{numberA} явлется четным числом");
+}
+else {
+     Console.WriteLine($"{numberA} явлется нечетным числом");
+}
